9660d787e71bca11e0db09f4f708221ee25b3c51
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2019-11-28 01:17:32 +0530

    container class basic

--- orchestrator/designPatterns/container.py
+++ orchestrator/designPatterns/container.py
@@ -1,18 +1,51 @@
+import docker
+client = docker.from_env()
+"""
+before this:
+pull repo
+port number of container within container
+name of image
+"""
+import socket
+def get_free_tcp_port():
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.bind(('', 0))
+    addr, port = tcp.getsockname()
+    tcp.close()
+    return port
+
 class Container:
-    def __init__(self):
-        #create docker image
-        #self id = id
-        #state?
-        print("Container created")
+    def __init__(self,image='flaskexample/flaskexample',containerPort=5000):
+        port=get_free_tcp_port()
+        container=client.containers.create(image,
+                                      detach=True,
+                                      ports={(str(containerPort)+'/tcp'): port}) # inside : outside
+        self.id=container.id
+        self.port=port
+        self.container=container
+        print("Created",self.port)
 
     def __del__(self):
-        #delete docker image
-        print("Container deleted")
+        #if not stopped stop
+        self.container.stop()
+        self.container.remove()
+        print("Deleted",self.port)
     
     def start(self):
+        self.container.start()
+        print("Started",self.port)
         #start container
-        pass
+        # pass
 
     def stop(self):
+        self.container.stop()
+        print("Stopped",self.port)
+
         #stop container
-        pass
+        # pass
+"""
+if __name__ == "__main__":
+    c=Container()
+    c.start()
+    c.stop()
+"""
