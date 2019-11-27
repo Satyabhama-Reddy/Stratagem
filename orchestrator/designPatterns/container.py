784dbe8befbe5d36311419cbcd5f21ada52370f7
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2019-11-27 21:59:54 +0530

    object pool active containers

---
+++ orchestrator/designPatterns/container.py
@@ -0,0 +1,18 @@
+class Container:
+    def __init__(self):
+        #create docker image
+        #self id = id
+        #state?
+        print("Container created")
+
+    def __del__(self):
+        #delete docker image
+        print("Container deleted")
+    
+    def start(self):
+        #start container
+        pass
+
+    def stop(self):
+        #stop container
+        pass
--- orchestrator/designPatterns/objectPool.py
+++ orchestrator/designPatterns/objectPool.py
@@ -1,3 +1,6 @@
+from container import Container
+# from activeContainers import ActiveContainers
+
 """
 Usage:
 - Import ContainerPool
@@ -10,59 +13,45 @@ from objectPool import ContainerPool
 
 if __name__=="__main__":
     minNoOfContainers = 3
-    pool = ContainerPool(minNoOfContainers)
+    maxNoOfContainers = 6
+    pool = ContainerPool(minNoOfContainers,maxNoOfContainers)
     container = pool.acquire()
-    pool.add(2)
-    pool.delete(3)
     pool.release(container)
 """
 
 class ContainerPool:
-    def __init__(self, noOfContainers):
-        self._containers = [Container() for _ in range(noOfContainers)]
+    def __init__(self, minContainers, maxContainers):
+        self._inactiveContainers = [Container() for _ in range(minContainers)]
+        self._activeContainers = []
+        self._minContainers = minContainers
+        self._maxContainers = maxContainers
 
     def acquire(self):
-        container = self._containers.pop()
+        container = self._inactiveContainers.pop()
+        if(len(self._inactiveContainers) == 0 and len(self._activeContainers) < self._maxContainers):
+            self.add(1)
+            #when to delete?
         container.start()
-        return 
+        self._activeContainers.append(container)
+        return container
 
     def release(self, container):
-        # when release is called we need to make sure that the container is not servicing any requests
+        # how to decide which container to release..?
+        # if i pop the last container in active containers..that might be serving requests
         container.stop()
-        self._containers.append(container)
+        self._activeContainers.remove(container)
+        self._inactiveContainers.append(container)
     
-    #additional functions that we need for scaling up/down that are not part of objec tpool design pattern
+    #additional functions that we need for scaling up/down that are not part of object pool design pattern
 
     def add(self, noOfAdditionalContainers):
         for i in range(noOfAdditionalContainers):
-            self._containers.append(Container())
+            self._inactiveContainers.append(Container())
         
 
     def delete(self, noOfContainers):
         # if the object is in the list then it is not being used as acquire pops it
         # so we need not worry about it being in the middle of a task
         for i in range(noOfContainers):
-            obj = self._containers.pop()
+            obj = self._inactiveContainers.pop()
             del obj
-        
-
-
-class Container:
-    def __init__(self):
-        #create docker image
-        #self id = id
-        #state?
-        print("Container created")
-
-    def __del__(self):
-        #delete docker image
-        print("Container deleted")
-    
-    def start(self):
-        #start container
-        pass
-
-    def stop(self):
-        #stop container
-        pass
-
