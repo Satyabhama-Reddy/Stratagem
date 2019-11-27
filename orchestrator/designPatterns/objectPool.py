34c9a3342ae1dc57ecea073654cc87a8ff8eeebd
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2019-11-27 19:37:26 +0530

    objectPool DP

---
+++ orchestrator/designPatterns/objectPool.py
@@ -0,0 +1,59 @@
+"""
+Usage:
+- Import ContainerPool
+- pass minimum number of containers needed
+"""
+
+"""
+Reference:
+from objectPool import ContainerPool
+
+if __name__=="__main__":
+    minNoOfContainers = 3
+    pool = ContainerPool(minNoOfContainers)
+    container = pool.acquire()
+    pool.add(2)
+    pool.delete(3)
+    pool.release(container)
+"""
+
+class ContainerPool:
+    def __init__(self, noOfContainers):
+        self._containers = [Container() for _ in range(noOfContainers)]
+
+    def acquire(self):
+        return self._containers.pop()
+
+    def release(self, container):
+        # when release is called we need to make sure that the container is not servicing any requests
+        self._containers.append(container)
+    
+    #additional functions that we need for scaling up/down that are not part of objec tpool design pattern
+
+    def add(self, noOfAdditionalContainers):
+        for i in range(noOfAdditionalContainers):
+            self._containers.append(Container())
+        
+
+    def delete(self, noOfContainers):
+        # if the object is in the list then it is not being used as acquire pops it
+        # so we need not worry about it being in the middle of a task
+        for i in range(noOfContainers):
+            obj = self._containers.pop()
+            del obj
+        
+
+
+class Container:
+    def __init__(self):
+        #create docker image
+        #self id = id
+        #state?
+        print("Container created")
+
+    def __del__(self):
+        #delete docker image
+        print("Container deleted")
+    
+    
+
