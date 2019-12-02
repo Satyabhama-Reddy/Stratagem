ddaebff5f689b65198d9955b4c3baa2c7dc8ca27
KavyaVarma <kavya.varma98@gmail.com>
2019-12-02 13:11:51 +0530

    Added context for strategy

---
+++ orchestrator/orchestratorExceptions.py
@@ -0,0 +1,14 @@
+class Error(Exception):
+	pass
+
+class InvalidMinimumContainers(Error):
+	pass
+
+class InvalidMaximumContainers(Error):
+	pass
+
+class InvalidContainerSelectionChoice(Error):
+	pass
+
+class InvalidScalingChoice(Error):
+	pass
